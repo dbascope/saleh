3b78cec7d364894c0a217ffbbe464ccc0fc81c96
Brayan Bustillos <bustillosbrayan@gmail.com>
2015-02-09 21:32:28 -0400

    commit inicial

---
+++ protected/config/database.php
@@ -0,0 +1,10 @@
+<?php
+
+// This is the database connection configuration.
+return array(
+	'connectionString' => 'mysql:host=localhost;dbname=mopar',
+	'emulatePrepare' => true,
+	'username' => 'root',
+	'password' => 'sample',
+	'charset' => 'utf8',
+);
